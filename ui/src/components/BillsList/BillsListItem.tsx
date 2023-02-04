87c71d9003ceb724394f3ddb58c5230d81c74afd
Yevhen Kifiak <evgart.kiev@gmail.com>
2023-02-04 12:26:05 +0200

    feat: strapi updated, mantine updated

--- ui/src/components/BillsList/BillsListItem.tsx
+++ ui/src/components/BillsList/BillsListItem.tsx
@@ -1,33 +1,43 @@
 import { Title, Text, Paper, Stack, Group } from '@mantine/core';
 import { format } from 'date-fns';
 import { memo, useMemo } from 'react';
-import { Bill } from '../../client/models/Bills';
+import { Bill, BillUtil } from '../../client/models/Bills';
 import { globalLabel } from '../../global/labels';
 import { Table } from '../Table';
+import { SimpleRowData } from '../Table/TableRow';
 
 export interface BillsListItemProps {
     bill: Bill;
 }
 
+interface FormattedUtil extends SimpleRowData {
+    name: string;
+    range: string;
+    value: number;
+    tariff: string;
+    cost: string;
+}
+
+const getFormattedUtil = (util: BillUtil): FormattedUtil => {
+    const unit = util.type.data.attributes.unit;
+    const name = util.type.data.attributes.label;
+    const range = `${util.from} → ${util.to} ${unit}`;
+    const value = util.to - util.from;
+    const tariff = `${util.tariff.data.attributes.cost} ${globalLabel.currency}/${unit}`;
+    const cost = `${util.cost} ${globalLabel.currency}`;
+
+    return {
+        name,
+        range,
+        value,
+        tariff,
+        cost,
+    };
+};
+
 export const BillsListItem = memo(({ bill }: BillsListItemProps) => {
     const formattedData = useMemo(
-        () =>
-            bill.attributes.utils.map((util) => {
-                const unit = util.type.data.attributes.unit;
-                const name = util.type.data.attributes.label;
-                const range = `${util.from} → ${util.to} ${unit}`;
-                const value = util.to - util.from;
-                const tariff = `${util.tariff.data.attributes.cost} ${globalLabel.currency}/${unit}`;
-                const cost = `${util.cost} ${globalLabel.currency}`;
-
-                return {
-                    name,
-                    range,
-                    value,
-                    tariff,
-                    cost,
-                };
-            }),
+        () => bill.attributes.utils.map(getFormattedUtil),
         [bill.attributes.utils]
     );
 
